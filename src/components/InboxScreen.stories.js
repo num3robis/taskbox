98278f74b63fe7566a6a468e961820b9e104b617
Pierre Leclaire <numrbs@gmail.com>
2022-04-28 17:54:00 +0200

    screens

---
+++ src/components/InboxScreen.stories.js
@@ -0,0 +1,28 @@
+import InboxScreen from "./InboxScreen.svelte";
+import { fireEvent, within } from "@storybook/testing-library";
+
+export default {
+  component: InboxScreen,
+  title: "InboxScreen",
+};
+
+const Template = (args) => ({
+  Component: InboxScreen,
+  props: args,
+});
+
+export const Default = Template.bind({});
+
+export const Error = Template.bind({});
+Error.args = {
+  error: true,
+};
+
+export const WithInteractions = Template.bind({});
+WithInteractions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Simulates pinning the first task
+  await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+  // Simulates pinning the third task
+  await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+};
